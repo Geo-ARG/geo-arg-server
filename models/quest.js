49f4df0f43951283f709993adeedc6af5f781a48
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-17 12:00:09 +0700

    Update models and create new migrations

--- models/quest.js
+++ models/quest.js
@@ -2,11 +2,12 @@
 module.exports = function(sequelize, DataTypes) {
   var Quest = sequelize.define('Quest', {
     questTitle: DataTypes.STRING,
-    task: DataTypes.STRING
+    task: DataTypes.STRING,
+    eventId: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Quest.belongsTo(Event)
       }
     }
   });
