257cba03f2037484319d484360c96e7ad5a65b9d
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-17 12:10:10 +0700

    Add dummy data to route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+let dummy = {
+  "id": 4,
+  "quote" "ARG semangaatttt"
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(dummy);
 });
 
 module.exports = router;
