2a11ff934a8ba74aa6f071fe38c95bdaae64c764
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-18 21:28:53 +0700

    Edit seeders

--- seeders/20170318134013-seed-users.js
+++ seeders/20170318134013-seed-users.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
-      { username: "user1", email: "user1@gmail.com", createdAt: new Date(), updatedAt: new Date() },
-      { username: "user2", email: "user2@gmail.com", createdAt: new Date(), updatedAt: new Date() },
-      { username: "user3", email: "user3@gmail.com", createdAt: new Date(), updatedAt: new Date() }
+      { username: "user1", email: "user1@gmail.com", totalScore: 0, createdAt: new Date(), updatedAt: new Date() },
+      { username: "user2", email: "user2@gmail.com", totalScore: 0, createdAt: new Date(), updatedAt: new Date() },
+      { username: "user3", email: "user3@gmail.com", totalScore: 0, createdAt: new Date(), updatedAt: new Date() }
     ])
   },
 
